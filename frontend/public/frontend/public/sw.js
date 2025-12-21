77af296137ee94bfaf63489207764a7b3b24d61d
Autobot <shairan2016@gmail.com>
2025-12-21 11:50:04 -0600

    Add service worker install and activate events

--- frontend/public/frontend/public/sw.js
+++ frontend/public/frontend/public/sw.js
@@ -1,1 +1,9 @@
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", () => {
+  self.clients.claim();
+});
+
 self.addEventListener("fetch", () => {});
